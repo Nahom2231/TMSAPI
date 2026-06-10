56a63f1b8ac9b769d3414c3b235924701e2435e8
Nahom2231 <nahom0462@gmail.com>
2026-06-11 01:06:08 +0300

    feat: install and configure Jwt Bearer authentication middleware

--- Program.cs
+++ Program.cs
@@ -1,19 +1,34 @@
-var builder = WebApplication.CreateBuilder(args); 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters=new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+          ValidateIssuer=true,
+          ValidateAudience = true,
+          ValidateLifetime=true,
+          ValidIssuer="https://localhost:7295",
+          ValidAudience="https://localhost:7295",
+          IssuerSigningKey= new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+              System.Text.Encoding.UTF8.GetBytes("YourSuperSecretkeyThatisLongEnough123")
+          )
+
+        };
+    });
 
 builder.Services.AddAuthorization();
-var app = builder.Build(); 
- 
-app.UseRouting(); 
- app.UseAuthentication(); 
- app.UseAuthorization(); 
-app.MapGet("/api/assessments/results", () => Results.Ok(new 
-{ 
-    courseCode = "CS-101",     studentId = "S-001", 
-    letterGrade = "A" 
-})).RequireAuthorization(); 
- 
+var app = builder.Build();
 
- 
-app.Run(); 
- 
- 
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapGet("/api/assessments/results", () =>
+{
+    return Results.Ok(new { message = "Protected data" });
+}).RequireAuthorization(); 
+
+app.Run();
