9b3811e763a7ae6bbe35d9469559057072df903b
Nahom2231 <nahom0462@gmail.com>
2026-06-11 23:02:31 +0300

    feat: configure request logging and security middleware pipeline in Program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-
+using TmsApi;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOptions<PaymentOptions>()
+    .BindConfiguration("Payments")
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
 builder.Services.AddSingleton<EnrollmentWorker>();
 builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
 builder.Services.AddControllers();
